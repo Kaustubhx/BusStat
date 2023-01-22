697f934ee15d692f266813f2b47a35087c768502
Kastubhx <prime.ganekar@gmail.com>
2023-01-23 00:02:41 +0530

    Add: Calcuation Of Point A To B

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,33 +1,58 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useEffect, useRef } from 'react'
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, ProviderPropType } from 'react-native-maps';
 import tw from 'twrnc';
-import { useSelector } from 'react-redux';
-import { selectDestination, selectOrigin } from '../slices/navSlices';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectDestination, selectOrigin, setTravelTimeInformation } from '../slices/navSlices';
 import MapViewDirections from 'react-native-maps-directions';
 import { REACT_APP_GOOGLE_MAPS_API_KEY } from "@env";
+
 
 const Map = () => {
 
     const origin = useSelector(selectOrigin);
     const destination = useSelector(selectDestination);
     const mapRef = useRef(null);
+    const dispatch = useDispatch();
 
 
-    // useEffect(() => {
-    //     if (!origin || !destination) {
-    //         return;
-    //     } else {
-    //         mapRef.current?.fitToSuppliedMarkers(["origin", "destination"], {
-    //             edgePadding: {
-    //                 top: 50,
-    //                 right: 50,
-    //                 bottom: 50,
-    //                 left: 50,
-    //             }
-    //         });
-    //     }
-    // }, [origin, destination]);
+    useEffect(() => {
+        if (!origin || !destination) return;
+
+        zoomOut = setTimeout(() => {
+            mapRef.current?.fitToSuppliedMarkers(["origin", "destination"], {
+                edgePadding: {
+                    top: 50,
+                    right: 50,
+                    bottom: 50,
+                    left: 50,
+                },
+                animated: true
+            })
+        }, 500)
+
+    }, [origin, destination])
+
+
+    useEffect(() => {
+        if (!origin || !destination) return;
+
+        const getTravelTime = async () => {
+            fetch(
+                `https://maps.googleapis.com/maps/api/distancematrix/json?origins=Seattle&destinations=San+Francisco&key=AIzaSyBYzyaruNy9FbkEKOrDbnQwelZ1DWdXIns`
+            ).then((res) => res.json())
+                .then(data => {
+                    // dispatch(setTravelTimeInformation(data))
+                    console.log(data)
+                    console.log(data.rows[0].elements[0])
+                    // console.log(origin.description)
+                    // console.log(destination.description)
+                })
+        };
+
+        getTravelTime();
+    }, [origin, destination, REACT_APP_GOOGLE_MAPS_API_KEY])
+
 
     return (
         <MapView
@@ -59,7 +84,7 @@ const Map = () => {
                     }}
                     title='Origin'
                     description={origin.description}
-                    identifier="origin"
+                    identifier={"origin"}
                 />
             )}
 
@@ -71,11 +96,15 @@ const Map = () => {
                     }}
                     title='Destination'
                     description={destination.description}
-                    identifier="destination"
+                    identifier={"destination"}
                 />
             )}
         </MapView>
     )
 }
+
+Map.propTypes = {
+    provider: ProviderPropType,
+};
 
 export default Map
